a06542b9458fa8972948f021a17069180c16210c
Chen, Gary <GaryChen@eaton.com>
2021-10-27 09:50:21 +0800

    fix： add ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/testSetUp.js'],
   testEnvironment: 'jsdom', // default: jsdom, 可配置： node
+  // "transform": { "^.+\\.(ts|tsx|js|jsx)?$": "ts-jest" }, // 配置这项会报错
   "transformIgnorePatterns": [
     "/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime).+(js|jsx)$",
   ]
